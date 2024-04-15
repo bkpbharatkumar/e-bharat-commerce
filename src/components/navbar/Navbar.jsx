b0f6f0cd2caa22883602216145ac4b12964521c0
bkpbharatparihar <bkpbharatparihar3970@gmail.com>
2024-04-15 12:38:18 +0530

    Fifth Commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
         </ul>
     )
     return (
-        <nav className="bg-pink-600 bg-opacity-95 sticky top-0 h-20 shadow-md shadow-gray-500">
+        <nav className="bg-pink-600 bg-opacity-95 sticky top-0 lg:h-20 md:h-44 sm:h-44 shadow-md shadow-gray-500">
             {/* main  */}
             <div className="lg:flex lg:justify-between items-center py-5 lg:px-3 ">
                 {/* left  */}
