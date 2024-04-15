cca5e18559fe30677804d226270818e3e8e9a857
bkpbharatparihar <bkpbharatparihar3970@gmail.com>
2024-04-15 12:50:34 +0530

    Seventh Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ import withMT from "@material-tailwind/react/utils/withMT";
  export default withMT({
    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
    theme: {
-     extend: {},
+     
+      screens: {
+        'xs': '320px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        'custom': {'min': '940px', 'max': '1051px'},
+      },
+   
    },
    plugins: [],
  });
